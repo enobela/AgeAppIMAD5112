bc10c962bdee99f0b56f13da68b3794d8f0a726d
RC_Student_lab <enobela@rosebankcollege.co.za>
2024-09-10 18:18:23 +0000

    changed

--- app/src/main/java/com/example/demotest/MainActivity.kt
+++ app/src/main/java/com/example/demotest/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // We are now going to push this new comment and see if it is loaded.
+
+    }
+    private fun NewMethod()
+    {
+
+
+
     }
 }
